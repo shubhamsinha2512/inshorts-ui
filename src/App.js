5f3625a03dca9a6d085830933cd93a04badd957d
Shubham Sinha <shubham.sinha2512@gmail.com>
2021-03-24 00:14:40 +0530

    all working | add animation when new article renders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Article from './components/Article/Article'
+import { Container } from 'react-bootstrap'
+
+import {useState} from 'react'
 
 function App() {
+  
+  const article = [{
+    image : 'image1.jpg',
+    heading : "Man tells Abhishek 'You've a beautiful wife you don't deserve', actor responds",
+    body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod pretium nibh nec accumsan. Mauris quis vulputate felis. Integer vel euismod felis. Nunc dapibus sapien a nibh venenatis, at faucibus mi rutrum. Quisque bibendum in enim ut varius. Sed pellentesque semper tempus. Proin eget magna efficitur, tincidunt elit eu, accumsan leo. Pellentesque sed sollicitudin nisl, vel scelerisque sem. ",
+    footer : 'Only you can give such replies!'
+  },
+  {
+    image : 'image2.jpg',
+    heading : "He was a big actor, don't want to take names!",
+    body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod pretium nibh nec accumsan. Mauris quis vulputate felis. Integer vel euismod felis. Nunc dapibus sapien a nibh venenatis, at faucibus mi rutrum. Quisque bibendum in enim ut varius. Sed pellentesque semper tempus. Proin eget magna efficitur, tincidunt elit eu, accumsan leo. Pellentesque sed sollicitudin nisl, vel scelerisque sem. ",
+    footer : 'I quickly got away!'
+  },
+  {
+    image : 'image3.jpg',
+    heading : "Asian Paints announces 'Cherish' as colour of the year 2021!",
+    body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod pretium nibh nec accumsan. Mauris quis vulputate felis. Integer vel euismod felis. Nunc dapibus sapien a nibh venenatis, at faucibus mi rutrum. Quisque bibendum in enim ut varius. Sed pellentesque semper tempus. Proin eget magna efficitur, tincidunt elit eu, accumsan leo. Pellentesque sed sollicitudin nisl, vel scelerisque sem. ",
+    footer : 'Cherish is enduring and uplifting!'
+  },
+  {
+    image : 'image4.jpg',
+    heading : "Manipal School of FinTech with Axis Bank opens admissions for 3rd batch",
+    body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod pretium nibh nec accumsan. Mauris quis vulputate felis. Integer vel euismod felis. Nunc dapibus sapien a nibh venenatis, at faucibus mi rutrum. Quisque bibendum in enim ut varius. Sed pellentesque semper tempus. Proin eget magna efficitur, tincidunt elit eu, accumsan leo. Pellentesque sed sollicitudin nisl, vel scelerisque sem. ",
+    footer : 'Become a part of our growth journey'
+  }
+]
+
+
+  const [articleIndex, setArticleIndex] = useState(0);
+  const [currentArticle, setCurrentArticle] = useState(article[articleIndex])
+  const [touchStart, setTouchStart] = useState(0)
+  const [touchEnd, setTouchEnd] = useState(0)
+
+
+  function handleTouchStart(e){
+    setTouchStart(e.targetTouches[0].clientY);
+  }
+
+  function handleTouchMove(e){
+    setTouchEnd(e.targetTouches[0].clientY);
+  }
+  
+  function handleTouchEnd(e){
+    if(touchStart - touchEnd > 150) {
+      if(articleIndex < article.length){
+        setArticleIndex(articleIndex => articleIndex+1);
+        setCurrentArticle(article[articleIndex]);
+      }else{
+        setArticleIndex(0);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" 
+          onTouchStart={handleTouchStart} 
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+      >
+        <Article article={currentArticle}  />
+      </div>
   );
 }
 
---
+++ src/components/Article/Article.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Article.css'
+
+function Article({article}) {
+
+    return (
+        <div className='article'>
+            <img className='articleImg' src={article.image} alt='article' />
+            <div className='articleMain'>
+                <h4 className='articleHeading'>
+                    {article.heading}
+                </h4>
+
+                <p className='articleBody'>
+                    {article.body}
+                </p>
+            </div>
+                <div className='articleFooter'>
+                    <h6 className='articleFooterLine'>
+                        {article.footer}
+                    </h6>
+                    Tap to read the entire report
+                </div>
+        </div>
+    )
+}
+
+export default Article
